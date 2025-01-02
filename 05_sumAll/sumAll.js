c765f7853c506078b0249eae0e9bcf5cc72a14da
Julian <108022596+j-victoriano@users.noreply.github.com>
2025-01-01 18:36:01 -0800

    add logic for 05 excercise test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
+
+    let a;
+    let b;
+    if (num1 < num2){
+        a = num1;
+        b = num2;
+    } else {
+        a = num2;
+        b = num1;
+    }
+
+    let sum = 0;
+    for (i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
