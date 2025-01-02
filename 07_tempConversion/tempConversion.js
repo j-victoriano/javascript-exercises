e4468806760eb430fc0a0eb6f6c2c34ef15192f4
Julian <108022596+j-victoriano@users.noreply.github.com>
2025-01-01 19:19:59 -0800

    add logic to 07 exercise test cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let conversion;
+  conversion = (num - 32) * (5/9);
+  if(conversion % 1 != 0){
+    return parseFloat(conversion.toFixed(1));
+  } else {
+    return conversion;
+  }
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let conversion;
+  conversion = (num * (9/5)) + 32;
+  if(conversion % 1 != 0){
+    return parseFloat(conversion.toFixed(1));
+  } else {
+    return conversion;
+  }
 };
 
 // Do not edit below this line
