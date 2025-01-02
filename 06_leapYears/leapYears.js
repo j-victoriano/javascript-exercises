b93d14f514eba48d61327801e96a4cecb6c539b0
Julian <108022596+j-victoriano@users.noreply.github.com>
2025-01-01 18:58:43 -0800

    add logic for 06 exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    let isCentury = num % 100 === 0;
+    let isDivisibleByFour = num % 4 === 0;
+    let isYearDivisibleByFourHundred = num % 400 === 0;
 
+    if (isDivisibleByFour && (!isCentury || isYearDivisibleByFourHundred)){
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
