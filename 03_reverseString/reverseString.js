a18f2f358de59d9add50907195f8f0002cfeca4b
Julian <108022596+j-victoriano@users.noreply.github.com>
2025-01-01 17:33:02 -0800

    add logic for exercise 03 test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reverse = string.split("");
 
+    let result = "";
+    for(let i = string.length - 1; i >= 0; i--){
+        result += reverse[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
